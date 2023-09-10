257fd03e5e9b85ceb1e62258e6c1401838a9db84
r-tamura <rtamura30@gmail.com>
2023-09-10 14:13:51 +0900

    ch2 grep-liteファイルから行を読み込む

--- ch2/grep-lite/src/main.rs
+++ ch2/grep-lite/src/main.rs
@@ -1,14 +1,31 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+use std::{fs::File, io::BufRead, io::BufReader};
+
+use clap::{Arg, Command};
+use regex::Regex;
 
 fn main() {
+    let args = Command::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::new("pattern")
+                .help("The pattern to search for")
+                .value_name("pattern")
+                .required(true),
+        )
+        .get_matches();
+
+    let pattern = args.get_one::<String>("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
     let f = File::open("Cargo.toml").unwrap();
     let reader = BufReader::new(f);
 
     for line in reader.lines() {
         let line = line.unwrap();
-        let len = line.len();
-        println!("{} {}", line, len);
+        let contains_substring = re.find(&line);
+        match contains_substring {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
     }
 }
