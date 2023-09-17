aafd187650b45feef0993df1abc1b93d0b2ed41c
r-tamura <rtamura30@gmail.com>
2023-09-17 12:42:13 +0900

    ch3 構造体でファイルの模型を作る 2

--- ch3/notquite-file/src/main.rs
+++ ch3/notquite-file/src/main.rs
@@ -1,8 +1,38 @@
+#[allow(unused_variables)]
+
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
 }
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(
+        name: &str,
+        data: &Vec<u8>,
+    ) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+
+    fn read(&mut self, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp  = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+}
+
+
 
 fn open(f: &mut File) -> bool {
     true
@@ -12,20 +42,22 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
-}
-
 fn main() {
-    let f1 = File{
-        name: String::from("f1.txt"),
-        data: Vec::new(),
-    };
+    let f3_data: Vec<u8> = vec![114, 117, 115, 116, 23];
+    let mut f3 = File::new_with_data(
+        "2.txt",
+        &f3_data,
+    );
 
-    let f1_name = &f1.name;
-    let f1_length = &f1.data.len();
+    let mut buffer : Vec<u8> = vec![];
 
-    println!("{:?}", f1);
-    println!("{} is {} bytes long", f1_name, f1_length);
+    open(&mut f3);
+    let f2_length = f3.read(&mut buffer);
+    close(&mut f3);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", f3.name, f2_length);
+    println!("{}", text)
 }
