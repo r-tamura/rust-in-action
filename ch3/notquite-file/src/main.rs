af1c64fed965a911f407edb70782a40fd81d1e86
r-tamura <rtamura30@gmail.com>
2023-09-17 15:45:58 +0900

    ch3 トレイとで共通の振る舞いを定義する

--- ch3/notquite-file/src/main.rs
+++ ch3/notquite-file/src/main.rs
@@ -1,13 +1,26 @@
-use rand::prelude::*;
+use std::fmt;
+use std::fmt::Display;
 
-fn one_in(denominator: u32) -> bool {
-    thread_rng().gen_ratio(1, denominator)
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter,) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
 }
 
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
+    state: FileState,
 }
 
 impl File {
@@ -15,6 +28,7 @@ impl File {
         File {
             name: String::from(name),
             data: Vec::new(),
+            state: FileState::Closed,
         }
     }
 
@@ -28,6 +42,9 @@ impl File {
     }
 
     fn read(&self, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
         let mut tmp  = self.data.clone();
         let read_length = tmp.len();
         save_to.reserve(read_length);
@@ -36,40 +53,44 @@ impl File {
     }
 }
 
-
-
-fn open(f: File) -> Result<File, String> {
-    if one_in(10_000) {
-        let err_msg = String::from("Permission denied");
-        return Err(err_msg);
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter,) -> fmt::Result {
+        write!(f, "<{}({})>", self.name, self.state)
     }
+}
+
+
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
     Ok(f)
 }
 
-fn close(f: File) -> Result<File, String> {
-    if one_in(10_000) {
-        let err_msg = String::from("Interrupted by signal!");
-        return Err(err_msg);
-    }
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Closed;
     Ok(f)
 }
 
 fn main() {
     let f_data: Vec<u8> = vec![114, 117, 115, 116, 23];
     let f = File::new_with_data(
-        "2.txt",
+        "test.txt",
         &f_data,
     );
 
     let mut buffer : Vec<u8> = vec![];
 
-    let f3 = open(f).unwrap();
-    let f2_length = f3.read(&mut buffer).unwrap();
-    let f3 = close(f3).unwrap();
+    if f.read(&mut buffer).is_err() {
+        println!("Error checking is working");
+    }
+
+    let f = open(f).unwrap();
+    let f_length = f.read(&mut buffer).unwrap();
+    let f = close(f).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
-    println!("{:?}", f3);
-    println!("{} is {} bytes long", f3.name, f2_length);
+    println!("{:?}", f);
+    println!("{}", f);
+    println!("{} is {} bytes long", f.name, f_length);
     println!("{}", text)
 }
