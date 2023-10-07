9b565a1be6a8ea11aad4c43db2919d50f66f50ab
r-tamura <rtamura30@gmail.com>
2023-10-07 12:14:25 +0900

    ch6 プロセスに自分自身のメモリアドレスをスキャンさせる
    -
    
    -

--- ch6/memoryscan/src/main.rs
+++ ch6/memoryscan/src/main.rs
@@ -1,14 +1,23 @@
+static GLOBAL: i32 = 1000;
+const A: i32 = 42;
+fn noop() -> *const i32 {
+    let noop_local = 12345;
+    &noop_local as *const i32
+}
+
 fn main() {
-    let mut n_nonzero = 0;
+    let local_str = "a";
+    let local_int = 123;
+    let boxed_str = Box::new('b');
+    let boxed_int = Box::new(42);
+    let fn_int = noop();
 
-    for i in 1..10000 {
-        let ptr = i as *const u8;
-        let byte_at_addr = unsafe { *ptr };
+    println!("GLOBAL: {:p}", &GLOBAL);
+    println!("local_str: {:p}", &local_str);
+    println!("local_int: {:p}", &local_int);
+    println!("boxed_str: {:p}", Box::into_raw(boxed_str));
+    println!("boxed_int: {:p}", Box::into_raw(boxed_int));
+    println!("fn_int: {:p}", fn_int);
+    println!("A: {:p}", &A);
 
-        if byte_at_addr != 0 {
-            n_nonzero += 1;
-        }
-    }
-
-    println!("nonzero bytes: {}", n_nonzero);
 }
